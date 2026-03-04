01b8901d1ff6974306b8cdae53d95044a5993f16
Nicolas Rock <nicolasrpessoa@gmail.com>
2026-03-04 10:24:20 -0300

    A well-created and structured report.

--- modules/pdf_generator.py
+++ modules/pdf_generator.py
@@ -8,7 +8,7 @@ def create_pdf_report(data, sheet_name, output_path):
     pdf = FPDF()
     pdf.set_auto_page_break(auto=True, margin=15)
     pdf.add_page()
-
+    
     logo_path = "assets\company\company_2.png" 
     if os.path.exists(logo_path):
         pdf.image(logo_path, x=85, y=2, w=40)
@@ -16,7 +16,7 @@ def create_pdf_report(data, sheet_name, output_path):
     else:
         print(f"Aviso: Logotipo {logo_path} não encontrado.")
         pdf.ln(10)
-    
+
     pdf.set_font("Arial", "B", 16)
     pdf.cell(190, 15, f"RELATORIO SEMANAL - {sheet_name}", ln=True, align='C')
     pdf.ln(5)
@@ -25,20 +25,35 @@ def create_pdf_report(data, sheet_name, output_path):
     pdf.set_fill_color(220, 220, 220)
     pdf.cell(85, 10, "MÁQUINA/PLACA", 1, 0, 'C', True)
     pdf.cell(65, 10, "OPERADORES", 1, 0, 'C', True)
-    pdf.cell(40, 10, "HORAS/KMS", 1, 1, 'C', True)
+    pdf.cell(40, 10, "HORAS/KM", 1, 1, 'C', True)
     
     pdf.set_font("Arial", "", 9)
     total_week = 0
+    
+    lista_vermelha = [
+        "RETROESCAVADEIRA MC01", 
+        "PC 210 - ESCAVADEIRA MC 13",
+        "MOBI - LOCADO"
+    ]
+
     for item in data:
-        machine_text = item['machine'][:45]
-        pdf.cell(85, 10, machine_text, 1)
-        pdf.cell(65, 10, item['operator'][:35], 1)
+        if item['machine'] in lista_vermelha or item['hours'] == 0:
+            pdf.set_text_color(255, 0, 0) 
+            pdf.set_font("Arial", "B", 9)
+        else:
+            pdf.set_text_color(0, 0, 0)  
+            pdf.set_font("Arial", "", 9)  
+
+        pdf.cell(85, 10, str(item['machine'])[:45], 1)
+        pdf.cell(65, 10, str(item['operator'])[:35], 1)
         pdf.cell(40, 10, f"{item['hours']:.2f}", 1, 1, 'R')
+        
         total_week += item['hours']
         
+    pdf.set_text_color(0, 0, 0)
     pdf.set_font("Arial", "B", 10)
     pdf.set_fill_color(240, 240, 240)
-    pdf.cell(150, 10, "TOTAL HORAS / KMS", 1, 0, 'L', True)
+    pdf.cell(150, 10, "TOTAL HORAS/KM", 1, 0, 'L', True)
     pdf.cell(40, 10, f"{total_week:.2f}", 1, 1, 'R', True)
     
     filename = f"Report_{sheet_name.replace(' ', '_')}.pdf"
